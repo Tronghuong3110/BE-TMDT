43a4fe50bd6789ebbda787e8ef3cacb9fe31d0f9
Tronghuong3110 <tronghuong2002@gmail.com>
2024-06-03 14:18:25 +0700

    update 3/6/2024

--- src/main/java/com/javatechie/dto/BrandDto.java
+++ src/main/java/com/javatechie/dto/BrandDto.java
@@ -1,7 +1,5 @@
 package com.javatechie.dto;
 
-import com.javatechie.entity.ItemEntity;
-import jakarta.persistence.Column;
 import lombok.Data;
 
 import java.util.List;
@@ -11,5 +9,5 @@ public class BrandDto {
     private Integer id;
     private String name;
     private String description;
-    private List<ItemDto> items;
+    private List<ProductDto> items;
 }
